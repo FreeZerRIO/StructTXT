1e38b23f700c7a4857912dd69e2d78266d3dcb93
FreeZerRIO <31537782+FreeZerRIO@users.noreply.github.com>
2017-12-17 21:24:00 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,11 +1,3 @@
-//
-//  main.c
-//  Struct
-//
-//  Created by Алексей Баринов on 10.12.2017.
-//  Copyright © 2017 Era Company. All rights reserved.
-//
-
 #include <stdio.h>
 #include <stdlib.h>
 struct Box {
@@ -19,7 +11,7 @@ int main(int argc, char **argv) {
     FILE *file = fopen(argv[1], "rb"); 
     
     if (file == NULL) {                 /* check if a file exists */
-        printf("FILE NOT FOUND \n\n");
+        printf("File not found \n\n");
         return 0;
     }
     while (fread(&pack, sizeof(struct Box), 1, file)) {
@@ -29,7 +21,7 @@ int main(int argc, char **argv) {
     fseek(file, 0, SEEK_END);   /* get the size in byte */
     long size = ftell(file);
     
-    printf("\n\n FILE SIZE IS %ld BYTES \n\n", size);
+    printf("\n\n File size: %ld bytes\n", size);
     fclose(file);
     return 0;
 }
